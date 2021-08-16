de42af08558d6ff8697763ea771c8e1578168351
Gevorg Ghazaryan <gevorg.ghazaryan@gatsoft.am>
2021-08-16 19:46:16 +0400

    final counter

--- src/App.js
+++ src/App.js
@@ -4,49 +4,68 @@ import MainComponent from "./components/mainComponent";
 import React from "react";
 
 export default class App extends Component {
-  
-  state = {
-    val1: '',
-    val2: '',
+  constructor(props) {
+    super(props);
+    this.state = {
+      val1: "",
+      val2: "",
+      btnIsTrue: true,
+    };
+    this.intervalId = null;
   }
 
-  grdon = () => {
+  startInterval = () => {
+    // if (this.intervalId) {
+    //   clearInterval(this.intervalId);
+    // }
+
+    this.setState({ btnIsTrue: false });
 
     const hel = setInterval(() => {
-
-      if(this.state.val1 === 0 && this.state.val2 === 0) {
-        alert('Time End')
-        clearInterval(hel)
+      if (this.state.val1 === 0 && this.state.val2 === 0) {
+        alert("Time End");
+        clearInterval(hel);
         return;
       }
 
-      if(this.state.val2 < 1) {
-        this.setState({val1: this.state.val1 - 1, val2: 60})
+      if (this.state.val2 < 1) {
+        this.setState({ val1: this.state.val1 - 1, val2: 60 });
       }
 
-      this.setState({val2: this.state.val2 - 1})
-      
-    }, 1000)
+      this.setState({ val2: this.state.val2 - 1 });
+    }, 1000);
 
-  } 
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+      };
+    });
+    this.intervalId = hel;
+  };
 
   update = (childValue, childValue1) => {
-    this.setState({val1: childValue, val2: childValue1})
-  }
+    this.setState({ val1: childValue, val2: childValue1 });
+  };
 
-  stopInterval = () => [
-    this.setState({val1: 0, val2: 0})
-  ]
+  stopInterval = () => {
+    this.setState({ val1: 0, val2: 0, btnIsTrue: true });
+    clearInterval(this.intervalId);
+  };
 
   render() {
-
+    const { val1, val2 } = this.state;
     console.log(this.state);
-    const { val1, val2 } = this.state
-
     return (
       <div className="App">
-        <MainComponent values={this.update} blyat={this.grdon} cease={this.stopInterval} />
-        <p>{val1} : {val2}</p>
+        <MainComponent
+          update={this.update}
+          startInterval={this.startInterval}
+          cease={this.stopInterval}
+          finalState={this.state.btnIsTrue}
+        />
+        <p>
+          {val1} : {val2}
+        </p>
       </div>
     );
   }
--- src/components/mainComponent.js
+++ src/components/mainComponent.js
@@ -1,39 +1,74 @@
-import { Component } from "react"
+import React, { Component } from "react";
 import "../App.css";
 
 export default class MainComponent extends Component {
-   
-        state = {
-            val1: 0,
-            val2: 0
-        }
-    
-        sendData = () => {
-            if(this.state.val1 >= 0 && this.state.val2 >= 0 && this.state.val1 + this.state.val2 > 0) {
-                this.props.values(this.state.val1, this.state.val2)
-                this.props.blyat()
-            }else {
-                  alert('Please Write Valid Numbers')
-                }
-            
-        }
+  constructor(props) {
+    super(props);
 
-        stopBtn = () => {
-           this.props.cease()
-        }
+    this.state = {
+      val1: "",
+      val2: "",
+    };
 
-    render() {
-        return (
-            <div className="content">
-                <button onClick={this.sendData}>Start</button>
-                <button onClick={this.stopBtn}>Stop</button> <br/>
-                <input type="number" onChange={(evt) => {
-                    this.setState({val1: Number(evt.target.value)})
-                }}></input>
-                <input type="number" onChange={(evt) => {
-                    this.setState({val2: Number(evt.target.value)})
-                }}></input>
-            </div>
-        )
+    this.inputRef = React.createRef();
+  }
+
+  sendData = () => {
+    if (
+      this.state.val1 >= 0 &&
+      this.state.val2 >= 0 &&
+      this.state.val1 + this.state.val2 > 0
+    ) {
+      this.props.update(this.state.val1, this.state.val2);
+      this.props.startInterval();
+    } else {
+      alert("Please Write Valid Numbers");
+      this.setState({ val1: 0, val2: 0 });
+      this.props.update(this.state.val1, this.state.val2);
     }
+  };
+
+  stopBtn = () => {
+    this.props.cease();
+    this.setState({ val1: 0, val2: 0 });
+  };
+
+  componentDidMount() {
+    this.inputRef.current.focus();
+    console.log(this.inputRef);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("prevProps", prevProps.finalState);
+    console.log("currentProps", this.props.finalState);
+    if (prevProps.finalState !== this.props.finalState) {
+      console.log("mainComponent state has changed.");
+    }
+  }
+
+  render() {
+    return (
+      <div className="content">
+        {this.props.finalState && (
+          <button onClick={this.sendData}>Start</button>
+        )}
+        <button onClick={this.stopBtn}>Stop</button> <br />
+        <input
+          type="number"
+          value={this.state.val1}
+          ref={this.inputRef}
+          onChange={(evt) => {
+            this.setState({ val1: Number(evt.target.value) });
+          }}
+        ></input>
+        <input
+          type="number"
+          value={this.state.val2}
+          onChange={(evt) => {
+            this.setState({ val2: Number(evt.target.value) });
+          }}
+        ></input>
+      </div>
+    );
+  }
 }
